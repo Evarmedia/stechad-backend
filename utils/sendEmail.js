b04e1ac1ede5a2b71b015d20ad2e3692c9e87d7b
Evarmedia <mishakmanuel@gmail.com>
2026-02-07 14:18:33 +0100

    fix: typo

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.EMAIL.USER,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // Old password changed
   },
   tls: {
